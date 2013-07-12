722c2eaff28ae4ec4bc73dc9a0b73628f6c123f5
adi <aditya@eddywebs.com>
2013-07-12 01:13:54 -0400

    part 1

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 var express = require('express');
-
+var fs = require('fs'); 
 var app = express.createServer(express.logger());
+
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
+
+fs.readFile('index.html', 'utf8', function(err, html){
+	if(err) throw err;
+	console.log(html);
+	});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
